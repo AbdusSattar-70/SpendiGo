e882b213e9013642a40e16d8ac67cd9d1b80ef3f
Abdus Sattar <sasorganicagro@gmail.com>
2023-07-18 07:00:21 +0600

    Modify user model for photo validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   has_many :categories, foreign_key: 'author_id', dependent: :destroy
   has_many :deals, foreign_key: 'author_id', dependent: :destroy
   has_one_attached :photo, dependent: :destroy
+  validates :photo, presence: true
   validates :name, presence: true, length: { maximum: 25 }
 end
