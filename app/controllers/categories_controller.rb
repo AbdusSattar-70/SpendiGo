b28fcaec2efb39d5c3b5d2a3add4a8d352928a08
Abdus Sattar <sasorganicagro@gmail.com>
2023-07-19 12:21:01 +0600

    Modify category and deal controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,68 +1,49 @@
 class CategoriesController < ApplicationController
-  before_action :set_category, only: %i[show edit update destroy]
+  before_action :authenticate_user!
 
-  # GET /categories or /categories.json
   def index
-    @categories = Category.all
+    @categories = current_user.categories
+    @category_totals = {}
+
+    @categories.each do |category|
+      @category_totals[category.id] = category.deals.sum(:amount)
+    end
+
+    @total_payment = @category_totals.values.sum
   end
 
-  # GET /categories/1 or /categories/1.json
-  def show; end
+  def show
+    @category = Category.find(params[:id])
+    @total = @category.deals.sum(:amount)
+    @deals = @category.deals.order(created_at: :desc)
+  end
 
-  # GET /categories/new
   def new
     @category = Category.new
   end
 
-  # GET /categories/1/edit
-  def edit; end
-
-  # POST /categories or /categories.json
   def create
     @category = Category.new(category_params)
     @category.author_id = current_user.id
-    respond_to do |format|
-      if @category.save
-        format.html { redirect_to category_url(@category), notice: 'Category was successfully created.' }
-        format.json { render :show, status: :created, location: @category }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @category.errors, status: :unprocessable_entity }
-      end
+    if @category.save
+      flash[:success] = 'Category succesfully added'
+      redirect_to categories_path
+    else
+      flash[:error] = 'Error: Category could not be added'
+      render :new
     end
   end
 
-  # PATCH/PUT /categories/1 or /categories/1.json
-  def update
-    respond_to do |format|
-      if @category.update(category_params)
-        format.html { redirect_to category_url(@category), notice: 'Category was successfully updated.' }
-        format.json { render :show, status: :ok, location: @category }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @category.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /categories/1 or /categories/1.json
   def destroy
+    @category = Category.find(params[:id])
     @category.destroy
-
-    respond_to do |format|
-      format.html { redirect_to categories_url, notice: 'Category was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    flash[:notice] = 'Category removed successfully'
+    redirect_to categories_path
   end
+
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
-  def set_category
-    @category = Category.find(params[:id])
-  end
-
-  # Only allow a list of trusted parameters through.
   def category_params
     params.require(:category).permit(:name, :icon)
   end
